c11221829bde8429d240a5974cbf5a04968b9d6e
Dingoeboy <30139493@tafe.wa.edu.au>
2026-03-17 17:05:43 +0800

    add script to file manager

--- Assets/scripts/FileManager.cs
+++ Assets/scripts/FileManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.IO;
+using JetBrains.Annotations;
 
 public class FileManager : MonoBehaviour
 {
@@ -11,6 +12,10 @@ public class FileManager : MonoBehaviour
 
         textFileName = "myData";//this will be the filename
         CreateNewTextFile(textFileName); //calling function i wrote myself 
+
+        WriteToFile(textFileName, "My favourite captain is Captain Picard.");
+        WriteToFile(textFileName, "My second favorite captain is Captain Kirk.");
+
     }
 
     /// <summary>
@@ -29,15 +34,67 @@ public class FileManager : MonoBehaviour
             File.WriteAllText(directoryPath, fileName + '\n');
 
         }
+        else
+        {
+            Debug.Log("file already exists. Not creating it");
+
+
+        }
+
+        
+    }// end of create new text file function
+
+    public void WriteToFile(string fileName, string data) 
+    {
+        string directoryPath = Application.dataPath + "/DataFiles/" + fileName + ".txt";
+
+        File.AppendAllText(directoryPath, data + "\n");
 
 
     }
 
 
+    public string[] ReadTextFileContents(string fileName)
+    {
+        string directoryPath = Application.dataPath + "/DataFiles/" + fileName + ".txt";
+        string[] textContents = new string[0];
+        //you have to check if the file exists
+        if(File.Exists(directoryPath) == true)
+        {
+            textContents = File.ReadAllLines(directoryPath);
+        }
+            return textContents;
 
 
+    }
+    public void CreateCSV(string fileName)
+    { // takes a filename
+      // check if it exists
+      // if not create it
 
+        string directoryPath = Application.dataPath + "/DataFiles/";
+        string filePath = directoryPath + fileName + ".csv";
 
+        // check if the folder if not create it
+        if (!Directory.Exists(directoryPath))
+        {
 
+            Directory.CreateDirectory(directoryPath);
+        }
 
-}
+        if (!File.Exists(filePath))
+        {
+
+            File.WriteAllText(filePath, "Name,Level \n");
+            Debug.Log("CSVe created with headers at: " + filePath);
+
+        }
+
+        else
+        {
+            Debug.Log("CSV file already exists" + filePath);
+
+        }
+
+    }
+}// end of class FileManager
